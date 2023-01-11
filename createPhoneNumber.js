0abc9a34503424274d4989254fabb3756a599130
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2023-01-11 18:46:13 +0545

    no change

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -3,4 +3,4 @@ function createPhoneNumber(numbers){
     // return `(${str.slice(0,3)}) ${str.slice(3,6)}-${str.slice(6,10)}`
     return numbers.slice(0,3);
 }
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9]))
